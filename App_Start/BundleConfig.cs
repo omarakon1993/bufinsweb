8ec1666682f9cef359ba3c7bb729277d547e7f8c
Omar Gutierrez <omarandresx2@hotmail.com>
2025-11-09 18:58:21 -0500

    Ajustes para publicacion

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -8,23 +8,18 @@ namespace bufinsweb
         // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            // Bundles para archivos locales en Assets
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Assets/js/bootstrap.bundle.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/aos").Include(
+                      "~/Assets/js/aos.js"));
 
-            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información sobre los formularios.  De esta manera estará
-            // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                      "~/Assets/css/bootstrap.min.css",
+                      "~/Assets/css/font-awesome.min.css",
+                      "~/Assets/css/aos.css",
+                      "~/Assets/css/inter-font.css"));
         }
     }
 }
